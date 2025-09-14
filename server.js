e1105ee9de920899b537dd7c1f8d4a92fae97c43
Satyam00915 <satyamupadhyay00915@gmail.com>
2025-09-14 13:37:54 +0530

    init_repo

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+})
