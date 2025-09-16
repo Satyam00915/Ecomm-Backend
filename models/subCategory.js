2285de0fdd90db54f14daa298bfa0186333bc480
Satyam00915 <satyamupadhyay00915@gmail.com>
2025-09-16 13:24:59 +0530

    ref

--- models/subCategory.js
+++ models/subCategory.js
@@ -1,12 +1,19 @@
+import { DataTypes } from "sequelize";
 import { sequelize } from "../config/dbConnect";
-import Category from "./categoryModel";
 
 const SubCategory = sequelize.define("SubCategory", {
   id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
   name: { type: DataTypes.STRING, allowNull: false },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Category",
+      key: "id",
+    },
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  },
 });
-
-Category.hasMany(SubCategory, { foreignKey: "categoryId" });
-SubCategory.belongsTo(Category, { foreignKey: "categoryId" });
 
 export default SubCategory;
